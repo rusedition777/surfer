cbbba023c2bf4beb02273c94aa657d91b5b5507a
rusedition <rusedition@mail.ru>
2024-06-27 10:34:32 +0700

    0.1.5 Добавлен отлов ошибок, изменен файл с прокси

--- main.py
+++ main.py
@@ -21,54 +21,59 @@ def surf_site(proxy):
         "--disable-software-rasterizer")
     options.add_argument(f'--proxy-server={proxy}')
     driver = webdriver.Chrome(options=options)
-    driver.get("https://genpasswd.ru")
-    time.sleep(7)
-
-    all_html = driver.find_element(By.TAG_NAME, "html")
-
-    for i in range(10):
-        all_html.send_keys(Keys.DOWN)
-    time.sleep(5)
-
     try:
-        driver.find_element(By.XPATH, '//*[@id="select_all"]').click()
-        time.sleep((random.randint(7, 25)))
-
-        kol_sim = driver.find_element(By.XPATH, '//*[@id="id_kol_sim"]')
-        kol_sim.clear()
-        kol_sim.send_keys(str(random.randint(8, 12)))
-
-        time.sleep((random.randint(7, 25)))
-
-        kol_vo = driver.find_element(By.XPATH, '//*[@id="id_kol"]')
-        kol_vo.clear()
-        kol_vo.send_keys(str(random.randint(10, 24)))
-
-        time.sleep((random.randint(7, 25)))
-
-        driver.find_element(By.XPATH, '/html/body/div/div/div[3]/form/div[2]/button').click()
-
-        time.sleep(random.randint(3, 7))
-        driver.find_element(By.XPATH, '/html/body/header/div/nav/div/button/span').click()
-        time.sleep((random.randint(3, 7)))
-        driver.find_element(By.XPATH, '// *[ @ id = "navbarNavDropdown"] / ul / li[3] / a').click()
+        driver.get("https://genpasswd.ru")
+        time.sleep(7)
 
         all_html = driver.find_element(By.TAG_NAME, "html")
+
         for i in range(10):
             all_html.send_keys(Keys.DOWN)
-            time.sleep(random.randint(1, 3))
+        time.sleep(5)
 
-        driver.find_element(By.XPATH, f'/ html / body / div / div / div[2] / div / div[{str(random.randint(1, 7))}] / div / div / div / div / a').click()
+        try:
+            driver.find_element(By.XPATH, '//*[@id="select_all"]').click()
+            time.sleep((random.randint(7, 25)))
 
-        time.sleep((random.randint(3, 7)))
-        all_html = driver.find_element(By.TAG_NAME, "html")
-        for i in range(15):
-            all_html.send_keys(Keys.DOWN)
-            time.sleep(random.randint(1, 3))
+            kol_sim = driver.find_element(By.XPATH, '//*[@id="id_kol_sim"]')
+            kol_sim.clear()
+            kol_sim.send_keys(str(random.randint(8, 12)))
 
-        time.sleep((random.randint(25, 34)))
-        driver.close()
-        driver.quit()
+            time.sleep((random.randint(7, 25)))
+
+            kol_vo = driver.find_element(By.XPATH, '//*[@id="id_kol"]')
+            kol_vo.clear()
+            kol_vo.send_keys(str(random.randint(10, 24)))
+
+            time.sleep((random.randint(7, 25)))
+
+            driver.find_element(By.XPATH, '/html/body/div/div/div[3]/form/div[2]/button').click()
+
+            time.sleep(random.randint(3, 7))
+            driver.find_element(By.XPATH, '/html/body/header/div/nav/div/button/span').click()
+            time.sleep((random.randint(3, 7)))
+            driver.find_element(By.XPATH, '// *[ @ id = "navbarNavDropdown"] / ul / li[3] / a').click()
+
+            all_html = driver.find_element(By.TAG_NAME, "html")
+            for i in range(10):
+                all_html.send_keys(Keys.DOWN)
+                time.sleep(random.randint(1, 3))
+
+            driver.find_element(By.XPATH, f'/ html / body / div / div / div[2] / div / div[{str(random.randint(1, 7))}] / div / div / div / div / a').click()
+
+            time.sleep((random.randint(3, 7)))
+            all_html = driver.find_element(By.TAG_NAME, "html")
+            for i in range(15):
+                all_html.send_keys(Keys.DOWN)
+                time.sleep(random.randint(1, 3))
+
+            time.sleep((random.randint(25, 34)))
+            driver.close()
+            driver.quit()
+        except Exception as e:
+            print(e)
+            driver.close()
+            driver.quit()
     except Exception as e:
         print(e)
         driver.close()
